81e115cbaf041f82742528e479a3f30baa1d9f1a
Serg <setum77@gmail.com>
2025-08-31 00:17:10 +0200

    update code

--- run_flask_db.sh
+++ run_flask_db.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd /var/www/
-source /var/www/scan_vpn/myenv/bin/activate
-exec python -m scan_vpn.flask_db
+
+# Переход в директорию проекта (!!!заменить user-name на свое)
+cd /home/user-name/scan_vpn
+
+# Если не используем uv, то активируем виртуальное окружение (раскомитить две строки ниже, 
+# поправивить название папки .venv, если нужно,  
+# закомитить строку "/home/user-name/.local/bin/uv run -- python flask_db.py") 
+# source .venv/bin/activate
+# exec python -m scan_vpn.flask_db
+
+# Запуск приложения через uv (!!!заменить user-name на свое)
+/home/user-name/.local/bin/uv run -- python flask_db.py
